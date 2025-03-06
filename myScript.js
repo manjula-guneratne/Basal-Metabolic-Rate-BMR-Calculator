03f719256d289b194b1d2dee27e58f24c6ad2947
Manjula Guneratne <mguneratne3@gmail.com>
2025-03-06 07:34:56 -0500

    CalBMR started

--- myScript.js
+++ myScript.js
@@ -1,3 +1,35 @@
+"use strict"
+
+window.addEventListener("load",function(){
+    // Metric form
+    let metricForm = this.document.forms.metricForm;
+    let gender = metricForm.elements.gender;
+
+    // Select the model list when the form opens
+    gender.focus();
+
+    // Add an event listener for every form element
+    for (let i=0; i<metricForm.elements.length; i++){
+        metricForm.elements[i].addEventListener("change", CalBMR)
+    }
+
+    // Calculator
+
+    function CalBMR(){
+        // Determine the selected gender
+        let gIndex = gender.selectedIndex;
+        let gValue = gender.options[gIndex].value;
+
+        // 
+    }
+
+    /*********************************************** */
+
+    // Imperial form
+    let imperialForm = this.document.forms.imperialForm;
+
+})
+
 function openPage(pageName, elmnt, color){
     var i, tabcontent, tablinks;
 
